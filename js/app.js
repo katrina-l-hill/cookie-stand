863b2621e12f18d89c6129db51edc605c5d8acda
Katrina L Hill <98134026+katrina-l-hill@users.noreply.github.com>
2022-02-14 14:26:55 -0800

    added objects for each shop location

---
+++ js/app.js
@@ -0,0 +1,46 @@
+'use strict'
+
+let seattle = {
+  minCustomer: 23,
+  maxCustomer: 65,
+  avgCookieCustomer: 6.3,
+  function randomNum(min,max) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+  }
+};
+
+let tokyo = {
+  minCustomer: 3,
+  maxCustomer: 24,
+  avgCookieCustomer: 1.2,
+  function randomNum(min,max) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+  }
+};
+
+let dubai = {
+  minCustomer: 11,
+  maxCustomer: 38,
+  avgCookieCustomer: 3.7,
+  function randomNum(min,max) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+  }
+};
+
+let paris = {
+  minCustomer: 20,
+  maxCustomer: 38,
+  avgCookieCustomer: 4.3,
+  function randomNum(min,max) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+  }
+};
+
+let lima = {
+  minCustomer: 2,
+  maxCustomer: 16,
+  avgCookieCustomer: 4.6,
+  function randomNum(min,max) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+  }
+};
