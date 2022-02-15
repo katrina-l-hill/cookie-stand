86ebd623ade3f22b67be1446c66162c8563b0bc4
Katrina L Hill <98134026+katrina-l-hill@users.noreply.github.com>
2022-02-14 16:54:30 -0800

    added code function to seattle

--- js/app.js
+++ js/app.js
@@ -1,46 +1,61 @@
 'use strict'
 
+// Step 1: Window in the DOM
+let storeLocationSection = document.getElementById('shop location');
+console.log(storeLocationSection);
+
 let seattle = {
+  cityName = `Seattle`,
   minCustomer: 23,
   maxCustomer: 65,
-  avgCookieCustomer: 6.3,
-  function randomNum(min,max) {
-    return Math.floor(Math.random() * (max - min + 1) + min);
+  avgCookiePerCust: 6.3,
+  randomCustHr: function(){
+    this.randomCustHr = `${randomCustHr}`;
+  // function randomCustHr(min,max) {
+    // return Math.floor(Math.random() * (max - min + 1) + min);
   }
 };
 
-let tokyo = {
-  minCustomer: 3,
-  maxCustomer: 24,
-  avgCookieCustomer: 1.2,
-  function randomNum(min,max) {
-    return Math.floor(Math.random() * (max - min + 1) + min);
-  }
-};
+// let tokyo = {
+//   cityName = `Tokyo`,
+//   minCustomer: 3,
+//   maxCustomer: 24,
+//   avgCookiePerCust: 1.2,
+//   randomCustHr: function(){
+//     this.randomCustHr = `${randomCustHr}`;
+//   // function randomCustHr(min,max) {
+//     // return Math.floor(Math.random() * (max - min + 1) + min);
+//   }
+// };
 
-let dubai = {
-  minCustomer: 11,
-  maxCustomer: 38,
-  avgCookieCustomer: 3.7,
-  function randomNum(min,max) {
-    return Math.floor(Math.random() * (max - min + 1) + min);
-  }
-};
+// let dubai = {
+//   cityName = `dubai`,
+//   minCustomer: 11,
+//   maxCustomer: 38,
+//   avgCookiePerCust: 3.7,
+//   randomCustHr: function(){
+//     this.randomCustHr = `${randomCustHr}`;
+//   // function randomCustHr(min,max) {
+//     // return Math.floor(Math.random() * (max - min + 1) + min);
+//   }
+// };
 
-let paris = {
-  minCustomer: 20,
-  maxCustomer: 38,
-  avgCookieCustomer: 4.3,
-  function randomNum(min,max) {
-    return Math.floor(Math.random() * (max - min + 1) + min);
-  }
-};
+// let paris = {
+//   cityName = `Paris`,
+//   minCustomer: 20,
+//   maxCustomer: 38,
+//   avgCookiePerCust: 4.3,
+//   // function randomCustHr(min,max) {
+//     return Math.floor(Math.random() * (max - min + 1) + min);
+//   }
+// };
 
-let lima = {
-  minCustomer: 2,
-  maxCustomer: 16,
-  avgCookieCustomer: 4.6,
-  function randomNum(min,max) {
-    return Math.floor(Math.random() * (max - min + 1) + min);
-  }
-};
+// let lima = {
+//   cityName = `Lima`,
+//   minCustomer: 2,
+//   maxCustomer: 16,
+//   avgCookiePerCust: 4.6,
+//   // function randomCustHr(min,max) {
+//     return Math.floor(Math.random() * (max - min + 1) + min);
+//   }
+// };
