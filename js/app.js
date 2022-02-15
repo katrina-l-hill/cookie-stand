83116f055c92551e3e7c8766eb0a84e6276c4752
Katrina L Hill <98134026+katrina-l-hill@users.noreply.github.com>
2022-02-14 22:01:38 -0800

    added outputs for Seattle and Tokyo

--- js/app.js
+++ js/app.js
@@ -7,7 +7,11 @@ console.log(storeLocationSection);
 console.log(storeLocationSection);
 console.dir(storeLocationSection);
 
-let storeHours = ['8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm']
+let storeHours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'];
+
+function randomCustPerHr(min,max) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
 
 let seattle = {
   cityName: 'Seattle',
@@ -16,14 +20,15 @@ let seattle = {
   avgCookiePerCust: 6.3,
   custPerHr: [],
   cookiesSoldPerHr: [],
-  custPerHr: function() {
-    this.custPerHr = `${custPerHr} customers per hour`;
+  populateData: function() {
+    for(let i = 0; i < storeHours.length; i++)
+    {
+      let custCount = randomCustPerHr(this.minCustomer, this.maxCustomer);
+      this.custPerHr.push(custCount);
+      this.cookiesSoldPerHr.push(Math.floor(custCount * this.avgCookiePerCust));
+    }
   }
 }
-  function randomCustPerHr(min,max) {
-    return Math.floor(Math.random() * (max - min + 1) + min);
- }
-
 
 seattle.render = function() {
   // Step 2 - create the element
@@ -32,21 +37,55 @@ seattle.render = function() {
   h2Elem.textContent = this.cityName;
   // Step 4 - add it to the DOM -- parent.appendChild(child)
   storeLocationSection.appendChild(h2Elem);
+  let dataList = document.createElement('ul');
+  for(let i = 0; i < storeHours.length; i++)
+  {
+    let item = document.createElement('li');
+    item.textContent = `${storeHours[i]}: ${this.cookiesSoldPerHr[i]} cookies`;
+    dataList.appendChild(item);
+  }
+  storeLocationSection.appendChild(dataList);
 }
 
-// let tokyo = {
-//   cityName = 'Tokyo',
-//   minCustomer: 3,
-//   maxCustomer: 24,
-//   avgCookiePerCust: 1.2,
-//   custPerHr: [],
-//   cookiesSoldPerHr: [],
-//   custPerHr: function(){
-//     this.custPerHr = `${custPerHr} customers per hour`;
-//   // function custPerHr(min,max) {
-//     // return Math.floor(Math.random() * (max - min + 1) + min);
-//   }
-// };
+seattle.populateData();
+seattle.render();
+
+let tokyo = {
+  cityName = 'Tokyo',
+  minCustomer: 3,
+  maxCustomer: 24,
+  avgCookiePerCust: 1.2,
+  custPerHr: [],
+  cookiesSoldPerHr: [],
+  populateData: function(){
+    for(let i = 0; i < storeHours.length;i++)
+  {
+    let custCount = randomCustPerHr(this.minCustomer, this.maxCustomer);
+    this.custPerHr.push(custCount);
+    this.cookiesSoldPerHr.push(Math.floor(custCount * this.avgCookiePerCust));
+  }
+  }
+}
+
+seattle.render = function() {
+  // Step 2 - create the element
+  let h2Elem = document.createElement('h2');
+  // Step 3 - Give it context if needed (optional)
+  h2Elem.textContent = this.cityName;
+  // Step 4 - add it to the DOM -- parent.appendChild(child)
+  storeLocationSection.appendChild(h2Elem);
+  let dataList = document.createElement('ul');
+  for(let i = 0; i < storeHours.length; i++)
+  {
+    let item = document.createElement('li');
+    item.textContent = `${storeHours[i]}: ${this.cookiesSoldPerHr[i]} cookies`;
+    dataList.appendChild(item);
+  }
+  storeLocationSection.appendChild(dataList);
+}
+
+tokyo.populateData();
+tokyo.render();
 
 // let dubai = {
 //   cityName = 'dubai',
